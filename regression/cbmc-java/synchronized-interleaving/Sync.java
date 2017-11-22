bfbd0c99c2182db102cfebb3fcdc3f071af09c1b
Dario Cattaruzza <dario.cattaruzza@hotmail.com>
2017-11-22 17:05:18 +0000

    Test for interleaving of synchronized blocks

---
+++ regression/cbmc-java/synchronized-interleaving/Sync.java
@@ -0,0 +1,83 @@
+import org.cprover.CProver;
+
+// This test checks that two synchronized blocks interleave (before and after)
+// and do not interfere with each other
+public class Sync {
+  
+  // Counter for testing interleavings
+  public int finalCounter=0;
+
+  // Counter for testing synchronization
+  public int testCounter=0;
+  
+  public boolean done=false;
+
+  // Records which thread was taken on the first pass.
+  public long firstPass=-1;
+  
+  public void test()
+  {
+    synchronized(this)
+    {
+      testCounter++;
+      assert (testCounter==1);
+      finalCounter++;
+      testCounter--;
+      if (finalCounter==1)
+        firstPass=CProver.getCurrentThreadID();
+      if (finalCounter==2)
+        done=true;
+    }
+  }
+  
+  public static void f() throws InterruptedException
+  {
+    boolean threadFirst;
+    final Sync o = new Sync();
+    Thread1 thread=new Thread1(o);
+    thread.start();
+    o.test();
+    if (o.finalCounter==1)
+      o.firstPass=0;
+    thread.join();
+    if (thread.done)
+    {
+      assert(o.done);
+//      assert(o.firstPass>0);
+      assert(o.finalCounter<=2);
+      /*if (o.firstPass==CProver.getCurrentThreadID())
+        threadFirst=true;
+      else
+        threadFirst=false;*/
+    }
+  }
+}
+
+class Thread1 extends Thread
+{
+  /** Reference to the object the thread operates on
+   * It is necessary because it represents the main synchronizing
+   * mechanism between threads.
+   */
+  Sync object;
+  
+  public boolean done=false;
+  
+  public Thread1(Sync object)
+  {
+    super();    
+    this.object = object;
+  }
+
+  /**
+   * The run function executes the test function specified for each thread
+   */
+  @Override
+  public void run ()
+  {
+    object.test();
+    if (object.finalCounter==1)
+      object.firstPass=1;
+    done=true;
+  }
+}
