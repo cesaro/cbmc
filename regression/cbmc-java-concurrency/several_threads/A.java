7e8483366ca80d32a39b5fd97b59848020fc8163
Cesar Rodriguez <cesar.rodriguez@diffblue.com>
2017-11-27 19:02:16 +0000

    (squash this) Improve tests for the static class init wrapper
    This commit should be squashed with the commit that introduced these regressionn
    tests.

--- regression/cbmc-java-concurrency/several_threads/A.java
+++ regression/cbmc-java-concurrency/several_threads/A.java
@@ -5,14 +5,14 @@ public class A
 {
 
   // expected verification success
-  public void me()
+  public static void me()
   {
     B b = new B();
     b.me();
   }
 
   // expected verification failure
-  public void me2()
+  public static void me2()
   {
     C c = new C();
     c.me();
@@ -30,6 +30,7 @@ public class A
     {
        D d = new D();
        d.start();
+       d.setX();
     }
   }
 }
@@ -51,27 +52,37 @@ class D extends Thread
   }
 }
 
+class Runit
+{
+  public static void main (String[] args)
+  {
+    //A.me();
+    A.me();
+  }
+}
+
 class B implements Runnable
 {
-  // FIXME: this assertion does not always hod as per the java spec (because x is not volatile),
-  // but cbmc currently doesn't reason about volatile variables
+  // FIXME: this assertion does not always hold as per the java spec (because x
+  // is not volatile), but cbmc currently doesn't reason about java volatile
+  // variables
   int x = 0;
   @Override
   public void run()
   {
     x += 1;
     int local_x = x;
-    assert(local_x == 1 || local_x == 2 || local_x == 3 ||
-      local_x == 10 || local_x == 11 || local_x == 12 || local_x == 13);
+    assert(local_x == 1 || local_x == 2 ||
+      local_x == 10 || local_x == 11 || local_x == 12);
   }
 
   // verification success
   public void me()
   {
-    Thread tmp = new Thread(this);
-    tmp.start();
-    tmp.start();
-    tmp.start();
+    Thread t1 = new Thread(this);
+    t1.start();
+    Thread t2 = new Thread(this);
+    t2.start();
     x = 10;
   }
 }
@@ -85,16 +96,17 @@ class C implements Runnable
   {
     x += 1;
     int local_x = x;
-    assert(local_x == 1 || local_x == 2 || local_x == 3);
+    assert(local_x == 1 || local_x == 2);
   }
 
-  // verification success
+  // verification fails because the assertion does not account for the writes of
+  // this thread
   public void me()
   {
-    Thread tmp = new Thread(this);
-    tmp.start();
-    tmp.start();
-    tmp.start();
+    Thread t1 = new Thread(this);
+    t1.start();
+    Thread t2 = new Thread(this);
+    t2.start();
     x = 10;
   }
 }
