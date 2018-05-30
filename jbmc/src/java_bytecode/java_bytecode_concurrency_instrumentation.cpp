205baefc6dc73c6fa749433269bbdacfd8e26554
Kurt Degiorgio <degiorgiokurt@outlook.com>
2018-05-30 15:05:17 +0100

    JBMC: Support for synchronized methods
    This commit adds support for synchronized methods by instrumenting all
    methods marked with the synchronization flag with calls to
    'monitorenter' and 'monitorexit'. These two methods are located in the
    java models library and implement a critical section.
    
    To this end the following changes are made:
    
    1. New irep_id, 'is_synchronized', to represent the synchronized keyword
       and appropriate changes to 'java_byecode_convert_method.cpp' to set
       this flag when a synchronized method is encountered.
    2. Setting of the 'is_static' flag when the method in question is static.
    3. Functions to find and instrument synchronized methods. Specifically,
       calls to "java::java.lang.Object.monitorenter:(Ljava/lang/Object;)V"
       and "java::java.lang.Object.monitorexit:(Ljava/lang/Object;)V" are
       respectively instrumented at the start and end of a synchronized
       method . Note, the former is instrumented at every point where
       the execution of the synchronized methods terminates. Specifically
       out of order return statements and exceptions.
    
    Note: instrumentation of synchronized methods is only triggered if the
          '--java-threading' command line option is specified.
    
    Note': instrumentation of synchronized methods requires the use of the
           java core models library as the locking mechanism is implemented
           in the model 'java.long.Object'.
    
    Note'': static synchronized methods are not supported yet.

--- jbmc/src/java_bytecode/java_bytecode_concurrency_instrumentation.cpp
+++ jbmc/src/java_bytecode/java_bytecode_concurrency_instrumentation.cpp
@@ -6,7 +6,7 @@ Author: Kurt Degiogrio, kurt.degiorgio@diffblue.com
 
 \*******************************************************************/
 
-#include "java_bytecode_convert_threadblock.h"
+#include "java_bytecode_concurrency_instrumentation.h"
 #include "expr2java.h"
 #include "java_types.h"
 
@@ -88,6 +88,171 @@ static const std::string get_thread_block_identifier(
   mp_integer lbl_id = binary2integer(expr.op0().get_string(ID_value), false);
   return integer2string(lbl_id);
 }
+
+/// Create a temporary variable
+/// /param symbol_table: a symbol table
+/// /param prefix: prefix of the name of the temporary variable
+/// /param name: base name of the temporary variable, to be append with "_tmp"
+/// /param type: type of the temporary variable
+/// /return returns new variable
+static symbol_exprt tmp_variable(
+  symbol_tablet &symbol_table,
+  const std::string &prefix,
+  const std::string &name,
+  const typet &type)
+{
+  irep_idt base_name = name + "_tmp";
+  irep_idt identifier = prefix + "::" + id2string(base_name);
+
+  auxiliary_symbolt tmp_symbol;
+  tmp_symbol.base_name = base_name;
+  tmp_symbol.is_static_lifetime = false;
+  tmp_symbol.mode = ID_java;
+  tmp_symbol.name = identifier;
+  tmp_symbol.type = type;
+  symbol_table.add(tmp_symbol);
+
+  symbol_exprt result(identifier, type);
+  result.set(ID_C_base_name, base_name);
+  return result;
+}
+
+/// Creates a monitorenter/monitorexit code_function_callt for
+/// the given synchronization object.
+/// \param symbol_table: a symbol table
+/// \param is_enter: Indicates whether we are creating a monitorenter or exit.
+/// \param object: An expression representing object  the object that need to
+/// perform a monitorenter/monitorexit operation
+static codet get_monitor_call(
+  symbol_tablet &symbol_table,
+  bool is_enter,
+  const exprt &object)
+{
+  code_typet type;
+  type.return_type() = void_typet();
+  type.parameters().resize(1);
+  type.parameters()[0].type() = java_reference_type(void_typet());
+  std::string symbol;
+
+  if(is_enter)
+    symbol="java::java.lang.Object.monitorenter:(Ljava/lang/Object;)V";
+  else
+    symbol="java::java.lang.Object.monitorexit:(Ljava/lang/Object;)V";
+
+  symbol_tablet::symbolst::const_iterator it
+    = symbol_table.symbols.find(symbol);
+
+  // If the functions for monitorenter or monitorexit do not exist
+  // (this can only happen if the java-models libary was not downloaded).
+  // We implement them as skips because symex would crash, since it cannot find
+  // the function in the symbol table
+  if(it == symbol_table.symbols.end())
+    return code_skipt();
+
+  // Otherwise we create a function call
+  code_function_callt call;
+  call.function() = symbol_exprt(symbol, type);
+  call.lhs().make_nil();
+  call.arguments().push_back(object);
+  return call;
+}
+
+/// Introduces a monitorexit before every return recursively
+/// \param code current element to check
+/// \param monitorexit codet to insert before the return.
+static void monitor_exits(
+  codet &code,
+  const codet &monitorexit)
+{
+  const irep_idt &statement=code.get_statement();
+  if(statement == ID_return)
+  {
+    // Relace the return with a monitor exit plus return block
+    code_blockt return_block;
+    return_block.add(monitorexit);
+    return_block.move_to_operands(code);
+    code = return_block;
+  }
+  else if((statement == ID_label)
+    || (statement == ID_block)
+    || (statement == ID_decl_block))
+  {
+    // If label or block found, explore the code inside the block
+    Forall_operands(it, code)
+    {
+      codet &sub_code = to_code(*it);
+      monitor_exits(sub_code, monitorexit);
+    }
+  }
+}
+
+/// Transforms the codet stored in in the value field of \p symbol. This
+/// expected to be a codet that needs to be synchronized, the transformation
+/// makes it thread-safe as described in the documentation of function
+/// convert_synchronized_methods
+///
+/// The resulting thread-safe codet is stored in the value field of \p symbol.
+///
+/// \param symbol_table: a symbol_table
+/// \param symbol: writeable symbol hosting code to synchronized
+/// \param sync_object: object to use as a lock
+static void instrument_synchronized_code(
+  symbol_tablet &symbol_table,
+  symbolt &symbol,
+  const exprt &sync_object)
+{
+    codet &code = to_code(symbol.value);
+
+    // Get interposition code for the monitor lock/unlock
+    codet monitorenter = get_monitor_call(symbol_table, true, sync_object);
+    codet monitorexit = get_monitor_call(symbol_table, false, sync_object);
+
+    // Create a unique catch label and empty throw type (i.e any)
+    // and catch-push them at the beginning of the code (i.e begin try)
+    code_push_catcht catch_push;
+    irep_idt handler("pc_synchronized_catch");
+    irep_idt exception_id;
+    code_push_catcht::exception_listt &exception_list=
+        catch_push.exception_list();
+    exception_list.push_back(
+      code_push_catcht::exception_list_entryt(exception_id, handler));
+
+    // Create a catch-pop to indicate the end of the try block
+    code_pop_catcht catch_pop;
+
+    // Create the finally block with the same label targeted in the catch-push
+    symbol_exprt catch_var =
+      tmp_variable(
+        symbol_table,
+        id2string(symbol.name),
+        "caught_synchronized_exception",
+        java_reference_type(symbol_typet("java::java.lang.Throwable")));
+    code_landingpadt catch_statement(catch_var);
+    codet catch_instruction = catch_statement;
+    code_labelt catch_label(handler, code_blockt());
+    code_blockt &catch_block = to_code_block(catch_label.code());
+    catch_block.add(catch_instruction);
+    catch_block.add(monitorexit);
+
+    // Re-throw exception
+    side_effect_expr_throwt throw_expr;
+    throw_expr.copy_to_operands(catch_var);
+    catch_block.add(code_expressiont(throw_expr));
+
+    // Write a monitorexit before every return
+    monitor_exits(code, monitorexit);
+
+    // Wrap the code into a try finally
+    code_blockt try_block;
+    try_block.move_to_operands(monitorenter);
+    try_block.move_to_operands(catch_push);
+    try_block.move_to_operands(code);
+    try_block.move_to_operands(catch_pop);
+    try_block.move_to_operands(catch_label);
+    code = try_block;
+}
+
+
 
 /// Transforms the codet stored in in \p f_code, which is a call to function
 /// CProver.startThread:(I)V into a block of code as described by the
@@ -284,7 +449,7 @@ static void instrument_getCurrentThreadID(
 ///
 /// Note': the ID of the thread is assigned after the thread is created, this
 /// creates bogus interleavings. Currently, it's not possible to
-/// assign the thread ID before the creation of the thead, due to a bug in
+/// assign the thread ID before the creation of the thread, due to a bug in
 /// symex. See https://github.com/diffblue/cbmc/issues/1630/for more details.
 ///
 /// \param symbol_table: a symbol table
@@ -360,4 +525,60 @@ void convert_threadblock(symbol_tablet &symbol_table)
       }
     }
   }
+}
+
+/// Iterate through the symbol table to find and appropriately instrument
+/// synchronized methods.
+///
+/// A synchronized method is a method with the synchronization keyword. Any
+/// code inside of such methods has to be thread-safe. To this end, a call to
+/// "java::java.lang.Object.monitorenter:(Ljava/lang/Object;)V" and
+/// "java::java.lang.Object.monitorexit:(Ljava/lang/Object;)V" is instrumented
+/// at the start and end of the method. Note, that the former is instrumented
+/// at every statement where the execution can exit the method in question.
+/// Specifically, out of order return statements and exceptions. The latter
+/// is dealt with by instrumenting a try catch block.
+///
+/// Note: current version cannot handle static methods.
+///
+/// Note': This method requires the availability of
+///   "java::java.lang.Object.monitorenter:(Ljava/lang/Object;)V" and
+///   "java::java.lang.Object.monitorexit:(Ljava/lang/Object;)V".
+///   (java-library-models)
+///
+/// \param symbol_table: a symbol table
+void convert_synchronized_methods(symbol_tablet &symbol_table)
+{
+  for(auto entry : symbol_table)
+  {
+    const symbolt &symbol = entry.second;
+
+    if(symbol.type.id() != ID_code)
+        continue;
+    if(symbol.value.is_nil())
+        continue;
+    if(symbol.type.get(ID_is_synchronized)!="1")
+        continue;
+
+    bool is_static=(symbol.type.get(ID_is_static)=="1");
+    if(is_static)
+      // FIXME:  handle static synchronized methods.
+      continue;
+
+    // Find the object to synchronize one
+    irep_idt this_id(id2string(symbol.name)+"::this");
+    exprt this_expr(this_id);
+
+    symbol_tablet::symbolst::const_iterator it
+      = symbol_table.symbols.find(this_id);
+
+    if(it == symbol_table.symbols.end())
+      // Failed to find object to synchronize on,
+      continue;
+
+    // get writeable reference and instrument the method
+    symbolt &w_symbol = symbol_table.get_writeable_ref(entry.first);
+    instrument_synchronized_code(
+      symbol_table, w_symbol, it->second.symbol_expr());
+   }
 }
