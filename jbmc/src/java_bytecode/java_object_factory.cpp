8fcbf62807752eddce5369cc52487208b03cef70
Kurt Degiorgio <degiorgiokurt@outlook.com>
2018-05-30 10:21:27 +0100

    JBMC: Removed @lock field from root class (usually: 'java.lang.Object')
    This is not needed any more as it has been superseded by a locking
    mechanism implemented in the java models library.
    
    Modified unit/regression tests to reflect the change.

--- jbmc/src/java_bytecode/java_object_factory.cpp
+++ jbmc/src/java_bytecode/java_object_factory.cpp
@@ -540,7 +540,7 @@ static codet make_allocate_code(const symbol_exprt &lhs, const exprt &size)
 /// cprover_associate_length_to_array_func(
 ///   *string_data_pointer, tmp_object_factory);
 /// arg = { .@java.lang.Object={
-///   .@class_identifier=\"java::java.lang.String\", .@lock=false },
+///   .@class_identifier=\"java::java.lang.String\" },
 ///   .length=tmp_object_factory,
 ///   .data=*string_data_pointer };
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -574,11 +574,10 @@ codet initialize_nondet_string_struct(
     ? "java::java.lang.String"
     : "java::" + id2string(struct_type.get_tag());
 
-  // @lock = false:
   const symbol_typet jlo_symbol("java::java.lang.Object");
   const struct_typet &jlo_type = to_struct_type(ns.follow(jlo_symbol));
   struct_exprt jlo_init(jlo_symbol);
-  java_root_class_init(jlo_init, jlo_type, false, class_id);
+  java_root_class_init(jlo_init, jlo_type, class_id);
 
   struct_exprt struct_expr(obj.type());
   struct_expr.copy_to_operands(jlo_init);
@@ -1071,10 +1070,6 @@ void java_object_factoryt::gen_nondet_struct_init(
     member_exprt me(expr, name, component_type);
 
     if(name=="@class_identifier")
-    {
-      continue;
-    }
-    else if(name=="@lock")
     {
       continue;
     }
--- jbmc/src/java_bytecode/java_root_class.cpp
+++ jbmc/src/java_bytecode/java_root_class.cpp
@@ -13,33 +13,15 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include "java_types.h"
 
-/*******************************************************************
-
- Function: java_root_class
-
- Inputs:
-
- Outputs:
-
- Purpose:
-
-\*******************************************************************/
-
+/// Create components to an object of the root class (usually java.lang.Object)
+/// Specifically, we add a new component, '@class_identifier'. This used
+/// primary to replace an expression like 'e instanceof A' with
+/// 'e.\@class_identifier == "A"'
+/// \param symbol: class symbol
 void java_root_class(symbolt &class_symbol)
 {
   struct_typet &struct_type=to_struct_type(class_symbol.type);
   struct_typet::componentst &components=struct_type.components();
-
-  {
-    // for monitorenter/monitorexit
-    struct_typet::componentt component;
-    component.set_name("@lock");
-    component.set_pretty_name("@lock");
-    component.type()=java_boolean_type();
-
-    // add at the beginning
-    components.insert(components.begin(), component);
-  }
 
   {
     // the class identifier is used for stuff such as 'instanceof'
@@ -56,13 +38,11 @@ void java_root_class(symbolt &class_symbol)
 /// Adds members for an object of the root class (usually java.lang.Object).
 /// \param jlo [out] : object to initialize
 /// \param root_type: type of the root class
-/// \param lock: lock field
 /// \param class_identifier: class identifier field, generally begins with
 ///        "java::" prefix.
 void java_root_class_init(
   struct_exprt &jlo,
   const struct_typet &root_type,
-  const bool lock,
   const irep_idt &class_identifier)
 {
   jlo.operands().resize(root_type.components().size());
@@ -71,10 +51,6 @@ void java_root_class_init(
   const typet &clsid_type=root_type.components()[clsid_nb].type();
   constant_exprt clsid(class_identifier, clsid_type);
   jlo.operands()[clsid_nb]=clsid;
-
-  const std::size_t lock_nb=root_type.component_number("@lock");
-  const typet &lock_type=root_type.components()[lock_nb].type();
-  jlo.operands()[lock_nb]=from_integer(lock, lock_type);
 
   // Check if the 'monitorCount' component exists and initialize it.
   // This field is only present when the java core models are embedded. It is
